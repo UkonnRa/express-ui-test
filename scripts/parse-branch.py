0a7cbcf82b96b5502da7bf0f6efca90c29f9ce18
Ukonn Ra <ukonnra@outlook.com>
2021-11-19 13:36:57 +0800

    build: main branch should not build docker images

--- scripts/parse-branch.py
+++ scripts/parse-branch.py
@@ -15,15 +15,23 @@ if run_id is None:
 
 BRANCH_FEATURE_PREFIX: Final = "refs/heads/feature/"
 BRANCH_RELEASE_PREFIX: Final = "refs/heads/release/"
-TAG_PREFIX = "refs/tags/"
+BRANCH_HOTFIX_PREFIX: Final = "refs/heads/hotfix/"
+BRANCH_BUGFIX_PREFIX: Final = "refs/heads/bugfix/"
+TAG_PREFIX: Final = "refs/tags/"
 if branch == "refs/heads/develop":
     print(f"{version}-staging-{run_id}")
 elif branch.startswith(BRANCH_FEATURE_PREFIX):
     name = branch[len(BRANCH_FEATURE_PREFIX):]
     print(f"{version}-f-{name}-{run_id}")
+elif branch.startswith(BRANCH_BUGFIX_PREFIX):
+    name = branch[len(BRANCH_BUGFIX_PREFIX):]
+    print(f"{version}-bug-{name}-{run_id}")
 elif branch.startswith(BRANCH_RELEASE_PREFIX):
     name = branch[len(BRANCH_RELEASE_PREFIX):]
-    print(f"{version}-rc-{run_id}")
+    print(f"{version}-rc-{name}-{run_id}")
+elif branch.startswith(BRANCH_HOTFIX_PREFIX):
+    name = branch[len(BRANCH_HOTFIX_PREFIX):]
+    print(f"{version}-rc-{name}-{run_id}")
 elif branch.startswith(TAG_PREFIX):
     name = branch[len(TAG_PREFIX):]
     if name != version:
