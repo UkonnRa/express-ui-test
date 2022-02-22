2c95b26a4621f386ef2e80b40c0b3fb0a35670ee
Ukonn Ra <ukonnra@outlook.com>
2022-02-22 20:44:41 +0800

    build(electron): fix 'install production dependencies' error by adding empty node_modules dir

--- scripts/build-desktop.ts
+++ scripts/build-desktop.ts
@@ -1,9 +1,12 @@
-import { copy } from 'fs-extra';
+import { copy, mkdir, pathExists } from 'fs-extra';
 import { execAsync } from './utils';
 
 const main = async () => {
   await execAsync('render', 'npm run build:desktop -w @white-rabbit/desktop-render');
   await copy('services/desktop-render/dist', 'services/desktop/dist');
+  if (!(await pathExists('services/desktop/node_modules'))) {
+    await mkdir('services/desktop/node_modules');
+  }
   await execAsync('desktop', 'npm run build:desktop -w @white-rabbit/desktop');
 };
 
